bb01e0dcca2b2ed7b4c93c517f4face0eda57dd3
Asmitta <tiwabrayan@gmail.com>
2025-02-23 15:29:18 +0100

    refactor: remove obsolete test files and configurations

--- Tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (!empty($_ENV['DB_FLAVOR'])) {
-	$_ENV['DB_DSN'] = $_ENV['DB_DSN_' . strtoupper($_ENV['DB_FLAVOR'])];
-}
--- src/AsmittaFormFlowBundle.php
+++ src/AsmittaFormFlowBundle.php
@@ -4,14 +4,20 @@ namespace Asmitta\FormFlowBundle;
 
 use Asmitta\FormFlowBundle\Util\TempFileUtil;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Asmitta\FormFlowBundle\Form\FormFlowInterface;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 /**
  * @author Christian Raue <christian.raue@gmail.com>
  * @copyright 2011-2024 Christian Raue
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
-class AsmittaFormFlowBundle extends Bundle
+class AsmittaFormFlowBundle extends AbstractBundle
 {
+
+	const FORM_FLOW_TAG = 'asmitta.form.flow';
 
 	/**
 	 * @return void
@@ -25,5 +31,21 @@ class AsmittaFormFlowBundle extends Bundle
 		register_shutdown_function(function (): void {
 			TempFileUtil::removeTempFiles();
 		});
+	}
+
+	public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+	{
+		// load an XML, PHP or YAML file
+		$container->import(__DIR__ . '/config/services.yaml');
+		$builder->registerForAutoconfiguration(FormFlowInterface::class)->addTag(self::FORM_FLOW_TAG);
+	}
+
+	public function process(ContainerBuilder $container)
+	{
+		$baseFlowDefinitionMethodCalls = $container->getDefinition(self::FORM_FLOW_TAG)->getMethodCalls();
+
+		foreach (array_keys($container->findTaggedServiceIds(self::FORM_FLOW_TAG)) as $id) {
+			$container->findDefinition($id)->setMethodCalls($baseFlowDefinitionMethodCalls);
+		}
 	}
 }
