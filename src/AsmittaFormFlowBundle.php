2a4a5434e17233d1048cc7f2bc3795a4b1128e3f
Asmitta <tiwabrayan@gmail.com>
2025-02-23 15:11:27 +0100

    refactor: restructured the project tree

---
+++ src/AsmittaFormFlowBundle.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Asmitta\FormFlowBundle;
+
+use Asmitta\FormFlowBundle\Util\TempFileUtil;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Christian Raue <christian.raue@gmail.com>
+ * @copyright 2011-2024 Christian Raue
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+class AsmittaFormFlowBundle extends Bundle
+{
+
+	/**
+	 * @return void
+	 */
+	public function boot()
+	{
+		/*
+		 * Removes all temporary files created while handling file uploads.
+		 * Use a shutdown function to clean up even in case of a fatal error.
+		 */
+		register_shutdown_function(function (): void {
+			TempFileUtil::removeTempFiles();
+		});
+	}
+}
---
+++ src/Exception/StepLabelCallableInvalidReturnValueException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Asmitta\FormFlowBundle\Exception;
+
+/**
+ * @author Christian Raue <christian.raue@gmail.com>
+ * @copyright 2011-2024 Christian Raue
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+class StepLabelCallableInvalidReturnValueException extends \RuntimeException {}
